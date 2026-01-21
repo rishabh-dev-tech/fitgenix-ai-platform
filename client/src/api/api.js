dd5d32190d9941acff16f4c5ce51271a8e563060
rishabh-dev-tech <rishabh8090496023@gmail.com>
2026-01-21 23:06:42 +0530

    replace localhost with live backend url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api"
+  baseURL: "https://fitgenix-ai-platform-1.onrender.com/api"
 });
 
 /* Attach token automatically */
